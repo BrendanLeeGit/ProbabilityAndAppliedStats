20b734bcd7dd508db49129538000113ccfcc79dc
BrendanLeeGit <brendanlee09090@gmail.com>
2023-02-06 04:25:42 -0500

    Adding a javadoc, but mainly using this to make sure that my correct GitHub account is being used.

--- src/SameBirthday/Person.java
+++ src/SameBirthday/Person.java
@@ -1,5 +1,9 @@
 package SameBirthday;
 
+/**
+ * Simple Person class, used to store a certain day for each person. Realistically could've just done an array though
+ * with the way I built this.
+ */
 public class Person {
     private int day;
     public Person (int day){
