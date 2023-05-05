2529c57b39ef019360bd64e4f0e358515fd466b5
BrendanLeeGit <brendanlee09090@gmail.com>
2023-05-04 22:52:44 -0400

    Forgot to change the commit message for my last few commits, whoops. Anyways, I'm just changing the function in Main.

--- src/ProjectII/CSVStuff/Main.java
+++ src/ProjectII/CSVStuff/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         FunctionOutput functionOutput = new FunctionOutput();
-        functionOutput.run(0, 8, 1, 5, 6);
+        functionOutput.run(0, 100, 1, 5, 6);
 
         //The smalter isn't required to run the program, there just won't be a FinalCSV.csv produced
         functionOutput.runSmalter(500, 3, 10);
